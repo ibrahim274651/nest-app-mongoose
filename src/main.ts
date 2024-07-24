866e2caa7af047aee71e695991df50a67cc542f9
ibrahim274651 <ikwizera@asystresources.com>
2024-07-24 09:25:24 +0200

    Swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 8000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ibrahim App')
+    .setDescription('Learning nestJs')
+    .setVersion('1.0')
+    .addServer('http://127.0.0.1:8000', 'Local environment')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/ibrahim-app', app, document);
+
+  mongoose.set('strictPopulate', false);
+
+  await app.listen(PORT);
 }
 bootstrap();
