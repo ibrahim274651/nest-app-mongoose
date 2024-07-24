127464606ffa5a63c4960a9f89d03ff5f26a36b7
ibrahim274651 <ikwizera@asystresources.com>
2024-07-24 08:46:56 +0200

    updated main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CategoriesModule } from './categories/categories.module';
-import { ProductsModule } from './products/products.module';
-import { ProfileModule } from './post/profile/profile.module';
-import { ProfileModule } from './profile/profile.module';
-import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -20,10 +14,6 @@ import { PostModule } from './post/post.module';
       }),
       inject: [ConfigService],
     }),
-    UsersModule,    CategoriesModule,
-    ProductsModule,
-    ProfileModule,
-    PostModule,
   ],
 })
 export class AppModule {}
--- src/products/dto/create-product.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
-export class CreateProductDto {
-  @IsNotEmpty({ message: 'Product name must be a non-empty string' })
-  name: string;
-  @IsNotEmpty({ message: 'Product price must be a non-empty price' })
-  price: number;
-}
